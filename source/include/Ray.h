0d15044c45aecceb41142fbca8bb7d283db723da
chenxu-whisper <1070669767@qq.com>
2025-09-14 14:21:54 +0800

    模拟漫反射

--- source/include/Ray.h
+++ source/include/Ray.h
@@ -7,6 +7,8 @@ struct Ray
 {
     glm::vec3 origin;
     glm::vec3 direction;
+
+    glm::vec3 hit(float t) const { return origin + t * direction; }
 };
 
 #endif //RAY_H
--- source/src/main.cpp
+++ source/src/main.cpp
@@ -14,14 +14,21 @@ int main()
     Film film(FILM_WIDTH, FILM_HEIGHT);
     Camera camera(film, glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), 90.0f);
     Sphere sphere(glm::vec3(0.0f, 0.0f, 0.0f), 0.5f);
-
+    glm::vec3 light_pos(3,3,2);
 
     thread_pool.parallelFor(film.getWidth(), film.getHeight(), [&](size_t x, size_t y)->void
     {
         Ray ray = camera.generateRay(glm::vec2(x, y), glm::vec2(0.5f, 0.5f)); // 生成射线
         std::optional<float> hit = sphere.intersect(ray); // 计算射线与球体的交点
         if (hit.has_value())
-            film.setPixel(x, y, glm::vec3(1.0f, 0.5f, 0.0f)); // 计算像素颜色
+        {
+            glm::vec3 hit_pos = ray.hit(hit.value());
+            glm::vec3 normal = glm::normalize(hit_pos - sphere.m_center);
+            glm::vec3 light_dir = glm::normalize(light_pos - hit_pos);
+            float diff = std::max(0.0f, glm::dot(normal, light_dir));
+            float ambient = 0.1f;
+            film.setPixel(x, y, glm::vec3(diff+ambient));
+        }
     });
 
     thread_pool.wait();
